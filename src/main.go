3408a6c41bfae0bd2f672c5262a0bcafb5a7671a
Peterlits Zo <peterlitszo@outlook.com>
2020-04-19 16:26:33 +0800

    split logic part from main.go

--- src/main.go
+++ src/main.go
@@ -4,7 +4,6 @@ import (
     "fmt"
     "io/ioutil"
     "strings"
-    "reflect"
     "bufio"
     "os"
 
@@ -35,94 +34,6 @@ func getFiles(dirpath string) (files []File, err error) {
         }
     }
     return files, nil
-}
-
-// ---[ return bool sreach's result ]------------------------------------------
-// return the AST's result.
-func AST_result(ast *typeAst, all_docID *DocList, wordmap map[string]*DocList) *DocList {
-    result := &DocList{}
-    // if the ast's len is zero, then return the full DocList
-    if ast == nil || len(*ast) == 0 {
-        result.Copy(all_docID)
-        return result
-    }
-    for _, expr_ptr := range *ast {
-        sub_result := EXPR_result(expr_ptr, all_docID, wordmap)
-        current := sub_result.start
-        for current != nil {
-            // get the union of all sub_result
-            result.AddDoc(current.docID)
-            current = current.next
-        }
-    }
-    return result
-}
-
-// return the result of expr:
-// --------------------------
-// return the expr's result. all atom is link by op 'and'
-func EXPR_result(expr *typeExpr, all_docID *DocList, wordmap map[string]*DocList) *DocList {
-    // if the expr's len is zero, then return empty DocList
-    result := &DocList{}
-    if len(*expr) == 0 {
-        return result
-    // else initial it by the first node
-    } else {
-        result.Copy(ATOM_result((*expr)[0], all_docID, wordmap))
-    }
-    for _, atom_ptr := range *expr {
-        sub_result := ATOM_result(atom_ptr, all_docID, wordmap)
-        current := result.start
-        // iter the sub-result
-        for current != nil {
-            // only need the node that the sub-result has
-            if !sub_result.Has(current.docID) {
-                result.RemoveDoc(current.docID)
-            }
-            current = current.next
-        }
-    }
-    return result
-}
-
-// return the result of atom:
-// --------------------------
-// atom's value is a interface, we need know that the type of the value. if the
-// type is typeAst, then it need call AST, else it should be a string, so we
-// need get the result by wordmap. if atom.not, then need to negate it by full
-// docID list.
-func ATOM_result(atom *typeAtom, all_docID *DocList, wordmap map[string]*DocList) *DocList {
-    result := &DocList{}
-    switch v := atom.value.(type) {
-    case string:
-        doclist_ptr, ok := wordmap[atom.value.(string)]
-        // if it have the key then copy else return empty list, else copy the 
-        if ok {
-            result.Copy(doclist_ptr)
-        }
-    case *typeAst:
-        sub_ast := atom.value.(*typeAst)
-        result.Copy(AST_result(sub_ast, all_docID, wordmap))
-    default:
-        // TODO: this is not OK( it look ugly )
-        fmt.Println("Error! the type is ", reflect.TypeOf(v))
-        return result
-    }
-    // negate the result
-    if atom.not {
-        current := all_docID.start
-        for current != nil {
-            // does not have? make it have
-            if !result.Has(current.docID) {
-                result.AddDoc(current.docID)
-            // does it have? remove it
-            } else {
-                result.RemoveDoc(current.docID)
-            }
-            current = current.next
-        }
-    }
-    return result
 }
 
 // ----------------------------------------------------------------------------
