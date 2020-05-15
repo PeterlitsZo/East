c17f3dd3e86d9a79610c1e418e73c40eb7f3a382
Peterlits Zo <peterlitszo@outlook.com>
2020-05-15 18:02:48 +0800

    rfc : (*) use Peterlits/argprase to get better output

--- makefile
+++ makefile
@@ -1,14 +1,16 @@
-main: ./src/parse.go ./src/list.go ./src/main.go ./src/split.go
+main: ./src/parse.go ./src/list.go ./src/main.go ./src/split.go ./src/argparse.go
 	go build -o main ./src
 
-./src/parse.go :
+./src/parse.go : ./src/parse.y
 	goyacc -o ./src/parse.go ./src/parse.y
 	rm y.output
 
 .PHONY: init
 init:
-	go get -u -v github.com/akamensky/argparse
+	go get -u -v github.com/PeterlitsZo/argparse
 
 .PHONY: clean
 clean:
+	rm ./main
+	rm ./index.dict
 	rm ./src/parse.go
--- src/argparse.go
+++ src/argparse.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-    "github.com/akamensky/argparse"
+    "github.com/PeterlitsZo/argparse"
     "os"
 )
+
 
 func EastArgparse() (parser *argparse.Parser, dirpath *string, command *string,
                 mkindex *bool, useindex *bool, interactive *bool,
@@ -13,34 +14,54 @@ func EastArgparse() (parser *argparse.Parser, dirpath *string, command *string,
     parser = argparse.NewParser("East", "sreach engine on file system")
 
     // initial the arg-parser
-    dirpath = parser.String("d", "dirpath",
-                             &argparse.Options{
-                                 Required: false,
-                                 Help: "the input files' folder path",
-                                 Default: "input",
-                             })
-    command = parser.String("c", "command",
-                             &argparse.Options{
-                                 Required: false,
-                                 Help: "the command to get the ID list (see README.pdf)",
-                                 Default: "",
-                             })
-    mkindex = parser.Flag("m", "mkindex",
-                           &argparse.Options{
-                               Help: "use this flag to make index named 'index.dict'",
-                           })
-    useindex = parser.Flag("u", "useindex",
-                            &argparse.Options{
-                                Help: "use file 'index.dict' to find result",
-                            })
-    interactive = parser.Flag("i", "interactive",
-                               &argparse.Options{
-                                   Help: "make self under the interactive mode",
-                               })
-    version = parser.Flag("v", "version",
-                           &argparse.Options{
-                               Help: "Show East's version",
-                           })
+    // 
+    // the input files' folder path
+    dirpath = parser.String(
+        "d", "dirpath",
+        &argparse.Options{
+            Required: false,
+            Help: "the input files' folder path",
+            Default: "input",
+    })
+
+    // the short command usage
+    command = parser.String(
+        "c", "command",
+        &argparse.Options{
+            Required: false,
+            Help: "the command to get the ID list (see README.pdf)",
+            Default: "",
+    })
+
+    // to make a index file or not
+    mkindex = parser.Flag(
+        "m", "mkindex",
+        &argparse.Options{
+            Help: "use this flag to make index named 'index.dict'",
+    })
+
+    // use the index file or not
+    useindex = parser.Flag(
+        "u", "useindex",
+        &argparse.Options{
+            Help: "use file 'index.dict' to find result",
+    })
+
+    // to use interactive or not
+    interactive = parser.Flag(
+        "i", "interactive",
+        &argparse.Options{
+            Help: "make self under the interactive mode",
+    })
+
+    // to show self's version
+    version = parser.Flag(
+        "v", "version",
+        &argparse.Options{
+            Help: "Show East's version",
+    })
+
+    // parse the argument, and if there is error, then raise it out
     err = parser.Parse(os.Args)
 
     // return the value
